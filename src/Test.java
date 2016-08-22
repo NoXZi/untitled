78f4ae702d1202c2349d7de5c9161458b610bc81
Alex <xpenx1234@gmail.com>
2016-08-22 16:45:49 +0300

    new catch
    new catch

--- src/Test.java
+++ src/Test.java
@@ -8,21 +8,38 @@ public class Test {
         ArrayList<String> name = new ArrayList<>();
         ArrayList<Integer> age = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        Integer countRecords = 3;
         boolean exit = false;
         try {
             while (exit == false) {
                 System.out.print("Введите имя ");
                 name.add(input.next());
+
                if ((name.get(name.size() - 1).compareTo("00") == 0) ) {
                    System.out.println("Ввод завершен");
                     exit = true;
-                } else {
-                   System.out.print("Введите возраст ");
-                   age.add(input.nextInt());
-               }
-            }
 
+
+                } else {if (name.get(name.size() - 1).matches("(.*[0-9].*)")){
+                    name.remove(name.size() - 1);
+                   System.out.println("Имя не может содержать цифр");
+
+                    /*if (name.get(name.size() - 1).indexOf('9') >= 0 ) {
+                       name.set(i, name.get(i).replaceAll("[^0-9]+", " "));
+                   System.out.println("Имя не может содержать цифр");
+                   exit = true;*/
+                    /*name.set(i,name.get(i).replaceAll(".*[0-9].*",(name.get(i))))*/
+               } else{
+
+                   System.out.print("Введите возраст ");
+                   age.add(input.nextInt());}
+
+
+
+               }
+
+
+
+               }
         } catch (InputMismatchException e) {
             System.out.println("Введены некоректные данные");
         } catch (IndexOutOfBoundsException e) {
@@ -34,11 +51,11 @@ public class Test {
             try {
                 name.size();
                 for (int i =0; i<name.size(); ++i)  {
-                    System.out.println(" Имя:  " + name.get(i) + "     Возраст:  " + age.get(i) );
+
+                    System.out.println(" Имя:  " + name.get(i)  + "     Возраст:  " + age.get(i) );
                 }
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Нету данных");
             }
-
     }
 }
